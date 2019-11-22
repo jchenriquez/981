46d5c4e6fc57d6643f9e59d724b42476233ded84
jchenriquez <jhenriquez2204@gmail.com>
2019-11-22 21:43:40 +0000

    Updated to use a balanced tree, in order to improve efficiency significantly.

--- main.go
+++ main.go
@@ -1,21 +1,176 @@
 package main
 
 import (
+  "math"
 	"fmt"
-	"sort"
 )
 
-type TimeValue struct {
-	Value string
-	Timestamp int
+type Node struct {
+  parent *Node
+  left *Node
+  right *Node
+  height int
+  timeStamp int
+  val interface {}
 }
 
-type ValuedArr struct {
-  TimeValues []TimeValue
-  LastInserted bool
+
+func updateTreeHeight(node *Node) {
+  if node.left == nil && node.right == nil {
+    node.height = 1
+  } else if node.right == nil {
+    node.height = node.left.height+1
+  } else if node.left == nil {
+    node.height = node.right.height+1
+  } else {
+    node.height = int(math.Max(float64(node.left.height), float64(node.right.height)))+1
+  }
 }
 
-type TimeMap map[string] ValuedArr
+func shiftLeft(node *Node) *Node {
+  rightSub := node.right
+  parent := node.parent
+
+  node.right = rightSub.left
+  rightSub.left = node
+
+  if parent != nil {
+    if parent.left == node {
+      parent.left = rightSub
+    } else {
+      parent.right = rightSub
+    }
+  }
+  
+  node.parent = rightSub
+  rightSub.parent = parent
+
+  updateTreeHeight(node)
+  updateTreeHeight(rightSub)
+
+  return rightSub
+}
+
+func shiftRight(node *Node) *Node {
+  leftSub := node.left
+  parent := node.parent
+
+  node.left = leftSub.right
+  leftSub.right = node
+
+  if parent != nil {
+    if parent.left == node {
+      parent.left = leftSub
+    } else {
+      parent.right = leftSub
+    }
+  }
+
+  node.parent = leftSub
+  leftSub.parent = parent
+
+  updateTreeHeight(node)
+  updateTreeHeight(leftSub)
+
+  return leftSub
+}
+
+func balance (tree *Node) *Node {
+
+  leftSub := tree.left
+  rightSub := tree.right
+
+  if leftSub == nil  {
+    if rightSub.height >= 2 {
+      tree = shiftLeft(tree)
+    } else {
+      updateTreeHeight(tree)
+    }
+  } else if rightSub == nil {
+    if leftSub.height >= 2 {
+      tree = shiftRight(tree)
+    } else {
+      updateTreeHeight(tree)
+    }
+  } else {
+    if math.Abs(float64(leftSub.height - rightSub.height)) >= 2 {
+      if leftSub.height > rightSub.height {
+        tree = shiftRight(tree)
+      } else {
+        tree = shiftLeft(tree)
+      }
+    } else {
+      updateTreeHeight(tree)
+    }
+  }
+  if tree.parent == nil {
+    return tree
+  }
+  return balance(tree.parent)
+}
+
+func (tr *Node) Insert (val interface{}, timeStamp int) *Node {
+  curNode := tr
+
+  for curNode != nil {
+    switch {
+      case timeStamp > curNode.timeStamp:
+        if curNode.right == nil {
+          curNode.right = &Node{curNode, nil, nil, 1, timeStamp, val}
+          return balance(curNode)
+        }
+        curNode = curNode.right
+      case timeStamp < curNode.timeStamp:
+        if curNode.left == nil {
+          curNode.left = &Node{curNode, nil, nil, 1, timeStamp, val}
+          return balance(curNode)
+        }  
+        curNode = curNode.left
+      default:
+        curNode.val = val
+        return tr
+    }
+  }
+
+  return tr
+}
+
+func (tr *Node) Search (timeStamp int) interface{} {
+  currNode := tr
+
+  for currNode != nil {
+    switch {
+      case currNode.timeStamp < timeStamp:
+        if currNode.right == nil {
+          return currNode.val
+        }
+        currNode = currNode.right
+      case currNode.timeStamp > timeStamp:
+        if currNode.left == nil {
+          parent := currNode.parent
+
+          for parent != nil {
+
+            if parent.right == currNode {
+              return parent.val
+            }
+
+            currNode = parent
+            parent = currNode.parent
+          }
+
+          return nil
+        }
+        currNode = currNode.left
+      default:
+        return currNode.val
+    }
+  }
+
+  return nil
+}
+
+type TimeMap map[string]*Node 
 
 
 /** Initialize your data structure here. */
@@ -24,53 +179,31 @@ func Constructor() TimeMap {
 }
 
 
-func (tm *TimeMap) Set(key string, value string, timestamp int)  {
-	valueArr, seen := (*tm)[key]
+func (tm *TimeMap) Set(key string, value string, timeStamp int)  {
+	tree, seen := (*tm)[key]
 
 	if !seen {
-		arr := make([]TimeValue, 0)
-    valueArr = ValuedArr{arr, true}
+		(*tm)[key] = &Node{nil, nil, nil, 1, timeStamp, value}
+    return
 	}
 
-	valueArr.TimeValues = append(valueArr.TimeValues, TimeValue{value, timestamp})
-	
-	(*tm)[key] = valueArr
+	(*tm)[key] = tree.Insert(value, timeStamp)
 }
 
 
-func (tm *TimeMap) Get(key string, timestamp int) string {
-	vA, in := (*tm)[key]
+func (tm *TimeMap) Get(key string, timeStamp int) string {
+	tree, in := (*tm)[key]
+  if in {
+    val := tree.Search(timeStamp)
 
-
-	if in {
-    if vA.LastInserted {
-      sort.Slice(vA.TimeValues, func(i, j int) bool {
-        return vA.TimeValues[i].Timestamp < vA.TimeValues[j].Timestamp
-      })
+    if val == nil {
+      return ""
     }
 
-    vA.LastInserted = false
-    (*tm)[key] = vA
+    return val.(string)
+  }
 
-		i := sort.Search(len(vA.TimeValues), func(i int) bool {
-			return vA.TimeValues[i].Timestamp >= timestamp
-		})
-
-		if i < len(vA.TimeValues) && vA.TimeValues[i].Timestamp == timestamp {
-			return vA.TimeValues[i].Value
-		} else {
-			i--
-
-			if i < 0 {
-				return ""
-			}
-
-			return vA.TimeValues[i].Value
-		}
-    
-	} else {
-		return ""
-	}
+  return ""
 }
 
 
@@ -85,11 +218,15 @@ func main() {
 	obj := Constructor()
 	obj.Set("love", "high", 10)
 	obj.Set("love", "low", 20)
-	fmt.Printf("%s\n", obj.Get("love", 5))
+  obj.Set("love", "low2", 30)
+  obj.Set("love", "low3", 40)
+  obj.Set("love", "low4", 50)
+  obj.Set("love", "low5", 60)
+	obj.Set("foo", "bar2", 4)
+	fmt.Printf("%s\n", obj.Get("foo", 5))
 	fmt.Printf("%s\n", obj.Get("love", 10))
 	fmt.Printf("%s\n", obj.Get("love", 15))
 	fmt.Printf("%s\n", obj.Get("love", 20))
 	fmt.Printf("%s\n", obj.Get("love", 25))
-	//obj.Set("foo", "bar2", 4)
-	//fmt.Printf("%s\n", obj.Get("foo", 5))
+	fmt.Printf("%s\n", obj.Get("foo", 5))
 }
